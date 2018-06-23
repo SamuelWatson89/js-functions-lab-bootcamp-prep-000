ecd6714d86036c84a0d8a71d4a7ff54273bb566c
Samuel Watson <otacon100@hotmail.com>
2018-06-23 18:44:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,16 +5,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  console.log (`Happy holidays, ${name}!`)
   return (`Happy holidays, ${name}!`)
 }
 
 function happyHolidayTo (holiday, name) {
-  console.log (`Happy ${holiday}, ${name}!`)
   return (`Happy ${holiday}, ${name}!`)
 }
 
 function holidayCountdown (holiday, days) {
-  console.log(`It's ${days} days until ${holiday}!`)
   return (`It's ${days} days until ${holiday}!`)
 }
